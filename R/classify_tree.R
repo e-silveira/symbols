13d4a6f708e1e5e6d5cc03e612291d30979a93d8
Eduardo da Silveira <esilveira@inf.ufsm.br>
2024-03-16 23:06:55 -0300

    Add options to the decision tree tab.

--- R/classify_tree.R
+++ R/classify_tree.R
@@ -6,8 +6,33 @@ ui_classify_tree <- function(id) {
     tagList(
         selectInput(
             inputId = NS(id, "method"),
-            label = "What method?",
+            label = "Select the splitting method:",
             choices = c("guess", "anova", "poisson", "class", "exp")
+        ),
+        checkboxInput(
+            inputId = "na_action",
+            label = "Delete missing observations."
+        ),
+        numericInput(
+            inputId = "minsplit",
+            label = "Specify the minimum split size:",
+            value = 20
+        ),
+        numericInput(
+            inputId = "minbucket",
+            label = "Specify the minimum bucket size:",
+            value = round(20 / 3)
+        ),
+        numericInput(
+            inputId = "cp",
+            label = "Specify the complexity:",
+            value = 0.01,
+            step = 0.01
+        ),
+        numericInput(
+            inputId = "maxdepth",
+            label = "Specify the maximum depth of the tree:",
+            value = 30
         )
     )
 }
