6f27130befc677c093b4e3c76629645048ef864e
Eduardo da Silveira <esilveira@inf.ufsm.br>
2024-03-16 23:36:35 -0300

    Add options as arguments to rpart.

--- R/classify_tree.R
+++ R/classify_tree.R
@@ -7,26 +7,26 @@ ui_classify_tree <- function(id) {
         selectInput(
             inputId = NS(id, "method"),
             label = "Select the splitting method:",
-            choices = c("guess", "anova", "poisson", "class", "exp")
+            choices = c("anova", "poisson", "class", "exp")
         ),
         numericInput(
-            inputId = "minsplit",
+            inputId = NS(id, "minsplit"),
             label = "Specify the minimum split size:",
             value = 20
         ),
         numericInput(
-            inputId = "minbucket",
+            inputId = NS(id, "minbucket"),
             label = "Specify the minimum bucket size:",
             value = round(20 / 3)
         ),
         numericInput(
-            inputId = "cp",
+            inputId = NS(id, "cp"),
             label = "Specify the complexity:",
             value = 0.01,
             step = 0.01
         ),
         numericInput(
-            inputId = "maxdepth",
+            inputId = NS(id, "maxdepth"),
             label = "Specify the maximum depth of the tree:",
             value = 30
         )
@@ -35,11 +35,32 @@ ui_classify_tree <- function(id) {
 
 server_classify_tree <- function(id, data) {
     moduleServer(id, function(input, output, session) {
+        observeEvent(input$minsplit, {
+            updateNumericInput(
+                inputId = "minbucket",
+                value = round(input$minsplit / 3)
+            )
+        })
+
+        observeEvent(input$minbucket, {
+            updateNumericInput(
+                inputId = "minsplit",
+                value = 3 * input$minbucket
+            )
+        })
+
         reactive({
             target <- colnames(data())[1]
             rpart(
                 formula = paste0(target, " ~ ."),
-                data = data()
+                data = data(),
+                method = input$method,
+                control = rpart.control(
+                    minsplit = input$minsplit,
+                    minbucket = input$minbucket,
+                    cp = input$cp,
+                    maxdepth = input$maxdepth
+                )
             ) |> rpart.plot()
         })
     })
